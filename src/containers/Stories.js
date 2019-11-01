a4d1ccbffcdad4cdd8e094d6f021dbc1b0f2a967
gregatgit <gregdd@outlook.com>
2019-11-01 17:03:58 +1000

    infinite scroll working

--- src/containers/Stories.js
+++ src/containers/Stories.js
@@ -1,22 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, memo } from 'react'
 
 import { getStoryIds } from '../services/hnApi'
 import { StoriesWrapper, GlobalStyle } from '../styles/StoriesStyles'
 import Story from '../components/Story'
+import { useInfinateScroll } from '../hooks/useInfinteScroll'
 
 function Stories() {
   const [storyIds, setStoryIds] = useState([])
+  const { count } = useInfinateScroll()
 
+  console.log('count: ', count)
   useEffect(() => {
     getStoryIds().then(data => setStoryIds(data))
   }, [])
 
   return (
     <div>
-    <GlobalStyle />
+      <GlobalStyle />
       <StoriesWrapper data-testid="stories-container">
         <h1>Hacker New Stories</h1>
-        {storyIds.map(storyId => (
+        {storyIds.slice(0, count).map(storyId => (
           <Story key={storyId} storyId={storyId} />
         ))}
       </StoriesWrapper>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,21 @@
+export const debounce = (func, wait, immediate, args) => {
+  let timeout
+
+  return () => {
+    const context = this
+    const callNow = immediate && !timeout
+    const later = () => {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+
+    if (callNow) func.apply(context, args)
+  }
+}
+
 export const selectFields = ({ id, by, url, time, title } = {}) => ({
   id,
   by,
@@ -7,34 +25,33 @@ export const selectFields = ({ id, by, url, time, title } = {}) => ({
 })
 
 export const mapTime = timestamp => {
-  const seconds = Math.floor((new Date() - timestamp * 1000) / 1000);
+  const seconds = Math.floor((new Date() - timestamp * 1000) / 1000)
 
-  let interval = Math.floor(seconds / 31536000);
-
-  if (interval > 1) {
-    return `${interval} years`;
-  }
-  interval = Math.floor(seconds / 2592000);
+  let interval = Math.floor(seconds / 31536000)
 
   if (interval > 1) {
-    return `${interval} months`;
+    return `${interval} years`
   }
-  interval = Math.floor(seconds / 86400);
+  interval = Math.floor(seconds / 2592000)
 
   if (interval > 1) {
-    return `${interval} days`;
+    return `${interval} months`
   }
-  interval = Math.floor(seconds / 3600);
+  interval = Math.floor(seconds / 86400)
 
   if (interval > 1) {
-    return `${interval} hours`;
+    return `${interval} days`
   }
-  interval = Math.floor(seconds / 60);
+  interval = Math.floor(seconds / 3600)
 
   if (interval > 1) {
-    return `${interval} minutes`;
+    return `${interval} hours`
+  }
+  interval = Math.floor(seconds / 60)
+
+  if (interval > 1) {
+    return `${interval} minutes`
   }
 
-  return `${Math.floor(seconds)} seconds`;
-};
-
+  return `${Math.floor(seconds)} seconds`
+}
